31771b912f0ac531b425f896cc8ab634ebd5dde3
remicdv <remi.de.verdiere@free.fr>
2020-05-05 11:38:10 +0200

    remove kino_dev app

--- kino_dev/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KinoDevConfig(AppConfig):
-    name = 'kino_dev'
--- kino_dev/templatetags/string_filter.py
+++
@@ -1,6 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def replace_underscore(string):
-    return string.replace('_', ' ')
